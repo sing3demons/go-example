544b3c705e2775dc35711716f0dc24204f345f96
sing3demons <sing3demons@live.com>
2023-08-27 08:47:25 +0700

    fix query

--- controllers/product.go
+++ controllers/product.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -9,6 +10,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type ProductController struct {
@@ -21,30 +23,54 @@ func NewProductController(db *mongo.Collection) *ProductController {
 
 func (p *ProductController) Find(c *gin.Context) {
 	products := []models.Product{}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	cursor, err := p.db.Find(ctx, bson.D{})
+	limit, err := strconv.Atoi(c.DefaultQuery("limit", "100"))
 	if err != nil {
 		c.JSON(500, gin.H{
 			"message": err.Error(),
 		})
 		return
 	}
+
+	search := c.Query("search")
+	opts := options.Find().SetLimit(int64(limit))
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	filter := bson.M{}
+
+	if search != "" {
+		filter = bson.M{"name": bson.M{"$regex": search}}
+	}
+
+	cursor, err := p.db.Find(ctx, filter, opts)
+	if err != nil {
+		c.JSON(500, gin.H{"message": err.Error()})
+		return
+	}
 	defer cursor.Close(ctx)
 
-	if err := cursor.All(ctx, &products); err != nil {
-		c.JSON(500, gin.H{
-			"message": err.Error(),
-		})
+	for cursor.Next(ctx) {
+		product := models.Product{}
+		if err := cursor.Decode(&product); err != nil {
+			c.JSON(500, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+		products = append(products, product)
+	}
+
+	total, err := p.db.CountDocuments(ctx, filter)
+	if err != nil {
+		c.JSON(500, gin.H{"message": err.Error()})
 		return
 	}
 
 	c.JSON(200, gin.H{
-		"data": products,
+		"total": total,
+		"data":  products,
 	})
-
 }
 
 func (p *ProductController) FindOne(c *gin.Context) {
