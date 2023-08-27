0ac1b90d4f388d823762ce3514c13cf3e902daab
sing3demons <sing3demons@live.com>
2023-08-27 12:47:41 +0700

    graceful shutdown

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/joho/godotenv v1.5.1
+	github.com/sirupsen/logrus v1.9.3
 	go.mongodb.org/mongo-driver v1.12.1
 )
 
--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/sing3demons/go-example/db"
 	"github.com/sing3demons/go-example/router"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
@@ -24,6 +31,35 @@ func main() {
 	r := gin.Default()
 	router.ProductRouter(r, mongo)
 
-	r.Run(":" + os.Getenv("PORT"))
+	ServeHttp(":"+os.Getenv("PORT"), "product-service", r)
+}
 
+func ServeHttp(addr, serviceName string, router http.Handler) {
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: router,
+	}
+
+	go func() {
+		logrus.Infof("[%s] http listen: %v", serviceName, srv.Addr)
+
+		if err := srv.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
+			logrus.Error("server listen err: ", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	logrus.Warn("shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		logrus.Fatal("server forced to shutdown: ", err)
+	}
+
+	logrus.Warn("server exited")
 }
